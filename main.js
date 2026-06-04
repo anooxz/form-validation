eb7fb71b772c9d035460d0b5eb8fc3af019806ed
anooxz <anooxz08@gmail.com>
2026-06-04 16:58:32 +0530

    Clean up commented code in main.js
    Removed commented-out code for phone number and confirm password validation.

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
 var inputName = document.getElementById("name")
 var inputEmail = document.getElementById("email")
 var inputPassword = document.getElementById("password")
-// var phoneNumber = document.getElementById("pnumber")
-// var cpassword = document.getElementById("cpassword")
 var submitBtn = document.getElementById("SubmitBtn")
 
 var nameError = document.getElementById("nameError")
 var emailError = document.getElementById("emailError")
 var passError = document.getElementById("passwordError")
-// var phError = document.getElementById("phError")
-// var cpassError = document.getElementById("cpasswordError")
 
 function validateForm() {
     let isValid = true
@@ -46,26 +42,6 @@ function validateForm() {
         passError.textContent = ""
     }
 
-    // if(cpassword.value !== inputPassword.value){
-    //     cpassword.style.border = "2px solid red";
-    //     cpassError.textContent = "Passwords do not match"
-    //     isValid = false
-    // } 
-    // else {
-    //     cpassword.style.border = "";
-    //     cpassError.textContent = ""
-    // }
-    
-
-    // if(phoneNumber.value.length !==10){
-    //     phoneNumber.style.border = "2px solid red";
-    //     phError.textContent="Enter 10 Numbers"
-    //     isValid=false
-    // }
-    //  else {
-    //     phoneNumber.style.border = "";
-    //     phError.textContent = ""
-    // }
     
 
     submitBtn.disabled = !isValid
@@ -75,8 +51,7 @@ function validateForm() {
 inputName.addEventListener("input", validateForm)
 inputEmail.addEventListener("input", validateForm)
 inputPassword.addEventListener("input", validateForm)
-// phoneNumber.addEventListener("input", validateForm)
-// cpassword.addEventListener("input",validateForm)
+
 
 submitBtn.addEventListener("click", function () {
     alert("Registration Successful!")
